461008a4f8d635c36250f7d8b4eab5e08d99f759
Warlen Pinheiro <warlen@live.com>
2020-11-04 01:01:44 -0300

    feat:ensure SignUpController returns 400 if no name is provided

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,7 @@ module.exports = {
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
+  //preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
